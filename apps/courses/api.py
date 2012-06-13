3314e7c74a9f3161f7bf8c15494fcfdd228a7b91
Scot Hacker <shacker@birdhouse.org>
2012-06-12 23:48:54 -0400

    Working sample implementation of an API for Profiles and CourseOfferings.

--- apps/courses/api.py
+++
@@ -1,8 +0,0 @@
-from tastypie.resources import ModelResource
-from courses.models import Offering
-
-
-class OfferingsResource(ModelResource):
-    class Meta:
-        queryset = Offering.objects.all()
-        resource_name = 'offerings'
